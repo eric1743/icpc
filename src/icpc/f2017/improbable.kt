383aa5bd5e3921f0944d855bfad74b695983e468
Benjamin.Erickson <Benjamin.Erickson@Target.com>
2018-08-24 13:11:54 -0500

    improbable input

--- src/icpc/f2017/improbable.kt
+++ src/icpc/f2017/improbable.kt
@@ -1,5 +1,16 @@
 package icpc.f2017
-//https://open.kattis.com/problems/improbable
-fun main(args: Array<String>) {
 
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+//https://open.kattis.com/problems/improbable
+
+fun main(args: Array<String>) {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    var line = br.readLine().split(" ").map { it.toInt() }
+    val r = line[0]
+    val c = line[1]
+    val map = List(r) { br.readLine().split(" ").map { it.toInt() } }
+
+        
 }
